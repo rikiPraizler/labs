b7aacb839ba69a3944d84dbcfd28a5f5cfd4efe9
rikiPraizler <150063960+rikiPraizler@users.noreply.github.com>
2024-03-26 20:13:05 +0000

    update tests

--- src/wallet/updateWallet.sol
+++ src/wallet/updateWallet.sol
@@ -1,57 +1,43 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-pragma solidity >=0.6.12 <0.9.0;
+contract Wallet {
+    address payable public owner;
+    mapping (address => uint256) public gabaim;
 
-///@title Wallet1
-
-contract Wallet1 {
-    address public owner;
-
-    address public user1;
-    address public user2;
-    address public user3;
-
+    // Constructor: Initializes contract with owner and predefined addresses
     constructor() {
-        //The owner receive who created the contract
         owner = payable(msg.sender);
-        user1 = 0x1234567890123456789012345678901234567890;
-        user2 = 0x9876543210987654321098765432109876543210;
-        user3 = 0x9876543210987654324554541312109876587570;
+        // Predefined addresses that can withdraw
+        gabaim[0xaC4E320Ed1235F185Bc6AC8856Ec7FEA7fF0310d] = 1;
+        gabaim[0x7a3b914a1f0bD991BAf826F4fE9a47Bb9880d25f] = 1;
+        gabaim[0x0638cF39b33D063c557AE2bC4B5D22a790Ac8Fe4] = 1;
     }
 
-    //Reserved function that transfer the funds
+    // Fallback function: Allows the contract to receive Ether
     receive() external payable {}
 
-    //only the owner can make this withdraw function
+    // Withdraw function: Allows owner and predefined addresses to withdraw Ether
     function withdraw(uint256 amount) external {
         require(
-            msg.sender == user1 ||
-                msg.sender == user2 ||
-                msg.sender == user3 ||
-                msg.sender == owner,
-            "you can't do this action"
+            owner == msg.sender || gabaim[msg.sender] == 1,
+            "Only the owner and predefined addresses are allowed to withdraw Ether"
         );
+        require(address(this).balance >= amount, "Insufficient balance");
         payable(msg.sender).transfer(amount);
     }
 
-    //return the contact's balance
-    function getBalance() external view returns (uint256) {
-        return address(this).balance;
+    function update(address oldGabai, address newGabai) public{
+    
+        require(owner == msg.sender, "Only the owner can update"); //only owner can update gabaaim
+        require(gabaim[oldGabai]==1,"the old gabai is not exist"); // check if gabbai exist in the hash
+        require(gabaim[newGabai]==0,"the gabai is exist"); // check if gabbai exist in the hash
+        gabaim[newGabai]=1;
+        gabaim[oldGabai]=0;
     }
 
-    function changeUser(address oldUser, address newUser) external {
-        require(msg.sender == owner, "you can't do this action");
-        if (user1 == oldUser) {
-            user1 = newUser;
-            return;
-        }
-        if (user2 == oldUser) {
-            user2 = newUser;
-            return;
-        }
-        if (user3 == oldUser) {
-            user3 = newUser;
-            return;
-        }
+    // Get Balance function: Returns the contract's balance
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
--- test/wallet/wallet.t.sol
+++ test/wallet/wallet.t.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import "forge-std/console.sol";
-import "@hack/wallet/wallet.sol";
-
+import "@hack/wallet/updateWallet.sol";
 
 contract WalletTest is Test {
     Wallet public w;
@@ -16,23 +15,24 @@ contract WalletTest is Test {
     }
 
     function testDeposit() public {
-
-        address randAdress= vm.addr(1111);
+        address randAdress = vm.addr(1111);
         vm.startPrank(randAdress);
         uint amount = 50;
         vm.deal(randAdress, amount);
         uint256 balanceBefore = address(w).balance;
-	    // Call the deposit function of the Wallet contract with 1 ether
+        // Call the deposit function of the Wallet contract with 1 ether
         payable(address(w)).transfer(10);
-	    uint256 balanceAfter = address(w).balance;
-	    assertEq(balanceAfter - balanceBefore, 10, "expect increase of 10 ether");
+        uint256 balanceAfter = address(w).balance;
+        assertEq(
+            balanceAfter - balanceBefore,
+            10,
+            "expect increase of 10 ether"
+        );
         assertEq(address(randAdress).balance, 40, "abcde");
         vm.stopPrank();
-        
     }
 
     function testWithdraw() public {
-
         address userAllow = 0xaC4E320Ed1235F185Bc6AC8856Ec7FEA7fF0310d;
         vm.startPrank(userAllow);
         uint amount = 50;
@@ -40,9 +40,99 @@ contract WalletTest is Test {
         uint256 balanceBefore = address(userAllow).balance;
         w.withdraw(5);
         uint256 balanceAfter = address(userAllow).balance;
-        assertEq(balanceBefore+5, balanceAfter);
+        assertEq(balanceBefore + 5, balanceAfter);
 
         vm.stopPrank();
+    }
 
+    function testWithdrawNotAllow() public {
+        address userNotAllow = 0x5ced660E3b925f034f99Df9466324F30A8Edf176;
+        vm.startPrank(userNotAllow);
+        uint amount = 50;
+        vm.deal(address(w), amount);
+        uint256 balanceBefore = address(userNotAllow).balance;
+        vm.expectRevert();
+        w.withdraw(5);
+        uint256 balanceAfter = address(userNotAllow).balance;
+        assertEq(balanceBefore, balanceAfter);
+
+        vm.stopPrank();
+    }
+
+    function testUpdateAllowed() public {
+        address owner = w.owner();
+        vm.startPrank(owner);
+
+        address oldGabai = 0xaC4E320Ed1235F185Bc6AC8856Ec7FEA7fF0310d;
+        address newGabai = 0x5ced660E3b925f034f99Df9466324F30A8Edf176;
+
+        // Call the update function with the old and new Gabai addresses
+        w.update(oldGabai, newGabai);
+
+        // Assert that the values in the gabaim mapping are updated as expected
+        assertEq(w.gabaim(newGabai), 1, "New Gabai not updated properly");
+        assertEq(w.gabaim(oldGabai), 0, "Old Gabai not updated properly");
+
+        vm.stopPrank();
+    }
+
+    function testUpdateNotAllowed() public {
+        //not allowed address
+        address add = 0xFc9ECA65d2BD19991F46ad73570920B2a7a23831;
+        vm.startPrank(add);
+
+        address oldGabai = 0xaC4E320Ed1235F185Bc6AC8856Ec7FEA7fF0310d;
+        address newGabai = 0x5ced660E3b925f034f99Df9466324F30A8Edf176;
+
+        vm.expectRevert();
+        // Call the update function with the old and new Gabai addresses
+        w.update(oldGabai, newGabai);
+
+        // Assert that the values in the gabaim mapping are updated as expected
+        // assertEq(w.gabaim(newGabai), 1, "New Gabai not updated properly");
+        // assertEq(w.gabaim(oldGabai), 0, "Old Gabai not updated properly");
+
+        vm.stopPrank();
+    }
+    function testUpdateExistGabai() public {
+        address owner = w.owner();
+        vm.startPrank(owner);
+
+        address oldGabai = 0xaC4E320Ed1235F185Bc6AC8856Ec7FEA7fF0310d;
+        address newGabai = 0x0638cF39b33D063c557AE2bC4B5D22a790Ac8Fe4;
+
+        vm.expectRevert();
+        // Call the update function with the old and new Gabai addresses
+        w.update(oldGabai, newGabai);
+
+        // Assert that the values in the gabaim mapping are updated as expected
+        // assertEq(w.gabaim(newGabai), 1, "New Gabai not updated properly");
+        // assertEq(w.gabaim(oldGabai), 0, "Old Gabai not updated properly");
+
+        vm.stopPrank();
+    }
+
+    function testUpdateNotExistOldGabai() public {
+        address owner = w.owner();
+        vm.startPrank(owner);
+
+        address oldGabai = 0xFc9ECA65d2BD19991F46ad73570920B2a7a23831;
+        address newGabai = 0x0638cF39b33D063c557AE2bC4B5D22a790Ac8Fe4;
+
+        vm.expectRevert();
+        // Call the update function with the old and new Gabai addresses
+        w.update(oldGabai, newGabai);
+
+        // Assert that the values in the gabaim mapping are updated as expected
+        // assertEq(w.gabaim(newGabai), 1, "New Gabai not updated properly");
+        // assertEq(w.gabaim(oldGabai), 0, "Old Gabai not updated properly");
+
+        vm.stopPrank();
+    }
+
+    function testGetBalance() public {
+        //how can I check this function?
+        w.getBalance();
+        // assertEq(address(w.balance),w.getBalance());
     }
 }
