25a01770c90793da4844fa502bf12aa574265259
rikiPraizler <riki4175045@gmail.com>
2024-04-07 14:56:19 +0300

    add my token

---
+++ new-project/src/new.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+ 
+contract RikisToken is ERC20 {
+    constructor() ERC20("MyToken", "RPS") {
+        this;
+    }
+}
