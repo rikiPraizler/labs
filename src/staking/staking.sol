c5cb5f1f78d5e568cbb3aad4fd812b5330a9e51d
rikiPraizler <riki4175045@gmail.com>
2024-04-08 17:28:26 +0300

    update staking

--- src/staking/staking.sol
+++ src/staking/staking.sol
@@ -7,8 +7,10 @@ contract Staking {
 
     IERC20 public immutable stakingToken;
     address public owner;
-    mapping (address => uint) public usersDeposit;
-    mapping (address => uint) public depositTime;
+    mapping (address => mapping (uint => uint)) public usersDeposit;
+    mapping (address => uint) public numOfDeposit;
+
+    // mapping (address => uint) public depositTime;
       
     uint public totalSupply = 1000000;
 
@@ -24,24 +26,67 @@ contract Staking {
         stakingToken.transfer(address(this),totalSupply);
 
     }
-
+    modifier reward(address user){
+       require(user!=address(0),"Address does not make sense");
+       _;
+    }
+    
     function deposit(uint amount) external {
         require(amount > 0 , "you must deposit an amount greater than 0" );
-        usersDeposit[msg.sender] += amount;
+        usersDeposit[msg.sender][block.timestamp] = amount;
         stakingToken.transfer(address(this), amount);
-        depositTime[msg.sender] = block.timestamp;
+        numOfDeposit[msg.sender] += 1;
     }
 
-    function withdraw(uint amount) external {
-        require(usersDeposit[msg.sender] >= amount , "you don't have this amount to withdraw");
-        require(block.timestamp > depositTime[msg.sender] + 1 weeks , "You can't withdraw the money yet");
-        uint balance = stakingToken.balanceOf(address(this)) - totalSupply;
-        uint percent = amount * 100 / balance;
-        stakingToken.transferFrom(address(this), msg.sender, totalSupply * percent / 100);
-        usersDeposit[msg.sender] -= amount;
-        totalSupply -=  totalSupply * percent / 100;
+    // function withdraw(uint amount) external {
+    //     require(amount > 0 , "the amount must be greater than 0" );
+    //     require(usersDeposit[msg.sender] != 0, "you didn't stake in this pool");
 
+    //     for (uint i = 0; i < numOfDeposit[msg.sender]; i++) {
+    //     uint time = usersDeposit[msg.sender][i];
+    //     if (time != 0 && block.timestamp > time + 1 weeks) {
+    //         uint depositedAmount = usersDeposit[msg.sender][time];
+    //         if (amount <= depositedAmount) {
+    //             deposit[msg.sender][time] -= amount;
+    //             totalWithdrawn += amount;
+    //             break; // Exit the loop if the full amount has been withdrawn
+    //         } else {
+    //             deposit[msg.sender][time] = 0;
+    //             totalWithdrawn += depositedAmount;
+    //             amount -= depositedAmount;
+    //         }
+    //     }
+    // }
+    function withdraw(uint amount)  external reward(msg.sender){
+        require(amount>0,"The amount must be greater than 0");
+
+        for (uint i = 0; i < numOfDeposit[msg.sender]&&amount!=0; i++) {
+            uint time = usersDeposit[msg.sender][i];
+            // Check if the timestamp is valid (not 0) and if it's been more than a week
+            require(block.timestamp > time + 1 weeks,"Time has not passed");
+            uint depositedAmount = usersDeposit[msg.sender][time];
+            if (amount <= depositedAmount) {
+                usersDeposit[msg.sender][time] -= amount;
+               amount=0;
+            } else {
+                usersDeposit[msg.sender][time] = 0;
+                amount -= depositedAmount;
+            }
+       }
+        require(amount==0,"you cant withdraw this amount");
+        uint balanceSupply=stakingToken.balanceOf(address(this))-totalSupply;
+        uint percentage = (amount * 100) /  balanceSupply;
+        stakingToken.transferFrom(address(this),msg.sender,(totalSupply * percentage)/100);
     }
+        // require(usersDeposit[msg.sender] >= amount , "you don't have this amount to withdraw");
+        // require(block.timestamp > depositTime[msg.sender] + 1 weeks , "You can't withdraw the money yet");
+        // uint balance = stakingToken.balanceOf(address(this)) - totalSupply;
+        // uint percent = amount * 100 / balance;
+        // stakingToken.transferFrom(address(this), msg.sender, totalSupply * percent / 100);
+        // usersDeposit[msg.sender] -= amount;
+        // totalSupply -=  totalSupply * percent / 100;
+
+    // }
 
     function getBalance() external view returns(uint){
         return stakingToken.balanceOf(address(this));
