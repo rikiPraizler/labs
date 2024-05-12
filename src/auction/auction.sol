8304b613b5aa0665b335cf9378edeb01a9442b33
rikiPraizler <150063960+rikiPraizler@users.noreply.github.com>
2024-05-12 17:24:13 +0000

    update

--- src/auction/auction.sol
+++ src/auction/auction.sol
@@ -10,59 +10,64 @@ contract Auction {
     NFTtoken public immutable nft;
     uint public finishTime;
     mapping(address => uint) public bidders;
-    address [] public addresses;
+    address[] public addresses;
     address public winner;
-    constructor(uint sum,address token,address n){
+    constructor(uint sum, address token, address n) {
         myToken = RikisToken(token);
         nft = NFTtoken(n);
         owner = msg.sender;
-        nft.mint(owner,5);
+        nft.mint(owner, 5);
         bidders[msg.sender] = sum;
         winner = msg.sender;
         finishTime = block.timestamp + 7 days;
         flagFinish = false;
     }
 
-    function Proposal(uint amount) external{  
-        require(amount > bidders[winner] || bidders[msg.sender] + amount > bidders[winner], "You need to put in more money to enter the auction");
-        if(block.timestamp < finishTime){
-            if(bidders[msg.sender] > 0)
-                bidders[msg.sender] += amount;            
-            else{
+    function Proposal(uint amount) external {
+        require(
+            amount > bidders[winner] ||
+                bidders[msg.sender] + amount > bidders[winner],
+            "You need to put in more money to enter the auction"
+        );
+        if (block.timestamp < finishTime) {
+            if (bidders[msg.sender] > 0) bidders[msg.sender] += amount;
+            else {
                 bidders[msg.sender] = amount;
                 addresses.push(msg.sender);
             }
-            myToken.transferFrom(msg.sender,address(this),amount);
+            myToken.transferFrom(msg.sender, address(this), amount);
             winner = msg.sender;
-        }
-        else if(!flagFinish){
+        } else if (!flagFinish) {
             flagFinish = true;
             finish();
-        } 
+        }
     }
 
-    function cancelation() external{
-        require(winner != msg.sender,"the winner cant cancel");
-        if(block.timestamp < finishTime){
-            myToken.transfer(msg.sender,bidders[msg.sender]);
+    function cancelation() external {
+        require(winner != msg.sender, "the winner cant cancel");
+        if (block.timestamp < finishTime) {
+            myToken.transfer(msg.sender, bidders[msg.sender]);
             bidders[msg.sender] = 0;
-        }
-        else if(!flagFinish){
-            flagFinish=true;
+        } else if (!flagFinish) {
+            flagFinish = true;
             finish();
-        } 
-    }
-
-    function finish() public{
-        require(winner != owner,"No one put money in");
-        for(uint i = 0; i < addresses.length; i++){
-            if(bidders[addresses[i]] > 0 && addresses[i] != winner)
-                myToken.transferFrom(address(this),addresses[i],bidders[addresses[i]]);
         }
-        nft.transferFrom(owner,winner,5);
     }
 
-    function getCurrentWinner() external view returns(uint){
+    function finish() public {
+        require(winner != owner, "No one put money in");
+        for (uint i = 0; i < addresses.length; i++) {
+            if (bidders[addresses[i]] > 0 && addresses[i] != winner)
+                myToken.transferFrom(
+                    address(this),
+                    addresses[i],
+                    bidders[addresses[i]]
+                );
+        }
+        nft.transferFrom(owner, winner, 5);
+    }
+
+    function getCurrentWinner() external view returns (uint) {
         return bidders[winner];
     }
 }
--- test/auction/auction.t.sol
+++ test/auction/auction.t.sol
@@ -6,8 +6,7 @@ import "forge-std/console.sol";
 import "@hack/auction/auction.sol";
 import "../../new-project/src/new.sol";
 
-
-contract AuctionTest is Test{
+contract AuctionTest is Test {
     Auction public a;
     address user;
     RikisToken public token;
@@ -19,47 +18,63 @@ contract AuctionTest is Test{
         user = vm.addr(1212);
     }
 
-    function testProposal() public{
+    function testProposal() public {
         vm.startPrank(user);
-        token.mint(address(user),100);
-        token.approve(address(a),80);
-        a.Proposal(80);                    
+        token.mint(address(user), 100);
+        token.approve(address(a), 80);
+        a.Proposal(80);
         address w = a.winner();
         assertEq(w, user, "error! the user is not the winner");
-        assertEq(token.balanceOf(address(user)), 20, "error! the amount not match");
-        assertEq(a.bidders(user),80,"error! the amount in the auction not match");
-        token.approve(address(a),20);
+        assertEq(
+            token.balanceOf(address(user)),
+            20,
+            "error! the amount not match"
+        );
+        assertEq(
+            a.bidders(user),
+            80,
+            "error! the amount in the auction not match"
+        );
+        token.approve(address(a), 20);
         a.Proposal(20);
         assertEq(w, user, "error! the user is not the winner");
-        assertEq(token.balanceOf(address(user)), 0, "error! the amount not match");
-        assertEq(a.bidders(user),100,"error! the amount in the auction not match");
+        assertEq(
+            token.balanceOf(address(user)),
+            0,
+            "error! the amount not match"
+        );
+        assertEq(
+            a.bidders(user),
+            100,
+            "error! the amount in the auction not match"
+        );
         vm.stopPrank();
     }
 
-    function testCancelation() public{
+    function testCancelation() public {
         vm.startPrank(user);
-        token.mint(address(user),50);
-        token.approve(address(a),50);
+        token.mint(address(user), 50);
+        token.approve(address(a), 50);
         a.Proposal(50);
         vm.stopPrank();
 
         address currentUser = vm.addr(434);
         vm.startPrank(currentUser);
-        token.mint(address(currentUser),160);
-        token.approve(address(a),160);
+        token.mint(address(currentUser), 160);
+        token.approve(address(a), 160);
         a.Proposal(160);
         vm.stopPrank();
 
         vm.startPrank(user);
-        token.approve(user,50);
+        token.approve(user, 50);
         a.cancelation();
         vm.stopPrank();
 
-        assertEq(token.balanceOf(address(user)),50,"error not mach money");
-        assertEq(token.balanceOf(address(a)),160,"error not mach money");
+        assertEq(token.balanceOf(address(user)), 50, "error not mach money");
+        assertEq(token.balanceOf(address(a)), 160, "error not mach money");
     }
 
-    function testWinnerCantCancel() public{
+    function testWinnerCantCancel() public {
         vm.startPrank(user);
         token.mint(address(user), 120);
         token.approve(address(a), 120);
@@ -69,8 +84,7 @@ contract AuctionTest is Test{
         vm.stopPrank();
     }
 
-
-    function testProposalOverTime() public{
+    function testProposalOverTime() public {
         vm.startPrank(user);
         token.mint(address(user), 120);
         token.approve(address(a), 120);
@@ -80,7 +94,7 @@ contract AuctionTest is Test{
         vm.stopPrank();
     }
 
-    function testFinish() public{
+    function testFinish() public {
         vm.startPrank(user);
         token.mint(address(user), 120);
         token.approve(address(a), 120);
@@ -89,7 +103,7 @@ contract AuctionTest is Test{
         // vm.expectRevert();
         // nft.approve(user, 5);
         a.cancelation();
-        assertEq(nft.balanceOf(address(user)),5,"errorrrrrrrrrrr");
+        assertEq(nft.balanceOf(address(user)), 5, "errorrrrrrrrrrr");
         vm.stopPrank();
     }
 }
