6893f73d6e7b60e66cd7272c70397646f1cdf9b4
rikiPraizler <riki4175045@gmail.com>
2024-05-19 16:28:32 +0300

    changeLending

--- src/lending/lending.sol
+++ src/lending/lending.sol
@@ -1,61 +1,99 @@
 // SPDX-License-Identifier: MIT
-// https://medium.com/@marketing.blockchain/how-to-create-a-multisig-wallet-in-solidity-cfb759dbdb35
+
 pragma solidity ^0.8.20;
 
-import "../../new-project/src/new.sol";
-import "../../new-project/src/NFT.sol";
+import "@hack/myCoin/MyERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-contract Lending {
+contract Lending{
     IERC20 public daiToken;
-    RikisToken public bondToken;
-    address public owner;
-    uint256 public rewardPerS;
-    // uint public wad = 10 ** 18;
-    uint256 public duration;
-    mapping(address => uint256) public daiDepositer;
-    mapping(address => uint256) public depositTime;
-    mapping(address => uint256) public rewards;
 
-    constructor(address bondT) {
+    MyERC20 public bondToken;
+
+    address public owner;
+
+    uint public rewardPerS;
+    uint public totalRewards;
+    uint public collateralRatio = 10 * 1e18 / 10;
+    uint public percentFee = (3 * 9999997) * 1e18 / 1000;
+
+    mapping (address => uint) public daiDeposits;
+    mapping (address => uint) public depositsTime;
+    mapping (address => uint) public rewards;
+    mapping (address => uint) public ethDeposits;
+    mapping (address => uint) public lendings;
+    mapping (address => uint) public lendingTime;
+    mapping (address => uint) public fees;
+
+
+    constructor(address bond) {
         owner = msg.sender;
         daiToken = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-        bondToken = RikisToken(bondT);
-        rewardPerS = 0.00000006 * 1e18;
+        bondToken = MyERC20(bond);
+        rewardPerS = 0.000000000002 * 1e18;
     }
 
     modifier updateReward(address user) {
-        if (daiDepositer[user] > 0) {
-            duration = block.timestamp - depositTime[user];
+        if(daiDeposits[user] > 0){
+            uint duration = block.timestamp - depositsTime[user];
             rewards[user] += (duration * rewardPerS) / 1e18;
-            depositTime[user] = block.timestamp;
+            depositsTime[user] = block.timestamp;
         }
         _;
     }
 
-    function depositDAI(uint256 amount) external updateReward(msg.sender) {
-        require(amount > 0, "you must deposit amount greater than 0");
+    modifier updateFee(address user){
+        if(lendings[user] > 0){
+            uint duration = block.timestamp - lendingTime[user];
+            fees[user] += (duration * (percentFee * lendings[msg.sender])) / 1e18;
+            lendingTime[user] = block.timestamp;
+        }
+        _; 
+    }
+
+    function depositDai(uint amount) external updateReward(msg.sender){
+        require(amount != 0, "amount equals 0");
         require(daiToken.balanceOf(msg.sender) >= amount);
-        daiDepositer[msg.sender] += amount;
+        daiDeposits[msg.sender] += amount;
         daiToken.transferFrom(msg.sender, address(this), amount);
         bondToken.mint(msg.sender, amount);
     }
 
-    function withdrawDAI(uint256 amount) external updateReward(msg.sender) {
-        require(amount > 0, "amount = 0");
-        require(amount <= rewards[msg.sender] + daiDepositer[msg.sender]);
+    function withdrawDai(uint amount) external updateReward(msg.sender){
+        require(amount != 0, "amount equals 0");
+        require(amount <= (rewards[msg.sender] + daiDeposits[msg.sender]));
         daiToken.transferFrom(address(this), msg.sender, amount);
-        if (rewards[msg.sender] < amount) {
+        if(rewards[msg.sender] < amount){
             amount -= rewards[msg.sender];
             rewards[msg.sender] = 0;
-            daiDepositer[msg.sender] -= amount;
+            daiDeposits[msg.sender] -= amount;
             bondToken.burn(msg.sender, amount);
-        } else {
+        }
+        else{
             rewards[msg.sender] -= amount;
         }
     }
+    receive() external payable{
+        ethDeposits[msg.sender] += msg.value;
+    }
+    
+    function lending(uint amount) external updateFee(msg.sender){
+        require(amount != 0, "amount equals 0");
+        require((amount * collateralRatio) / getPrice(0x2a1530C4C41db0B0b2bB646CB5Eb1A67b7158667) <= ethDeposits[msg.sender]);
+        require(amount <= daiToken.balanceOf(address(this)), "there isn't enough dai in the pool");
+        daiToken.transferFrom(address(this), msg.sender, amount);
+        lendings[msg.sender] += amount;
+    }
 
-    function depositETH(uint256 amount) external {}
-
-    function withdrawETH() external {}
+    function loanRepayment(uint amount) external updateFee(msg.sender){
+        require(amount != 0, "amount equals 0");
+        require(amount == lendings[msg.sender] + fees[msg.sender], "your loan is greater");
+        totalRewards += fees[msg.sender];
+        fees[msg.sender] = 0;
+        lendings[msg.sender] = 0;
+        daiToken.transferFrom(msg.sender, address(this), amount);
+    }
+    function getPrice(address token) public returns (uint256 amount) {
+        require(token == address(daiToken) || token == 0x2a1530C4C41db0B0b2bB646CB5Eb1A67b7158667, "invalid-token"); // DAI or ETH
+        amount = token == address(daiToken) ? 3000 : 1;
+    }
 }
